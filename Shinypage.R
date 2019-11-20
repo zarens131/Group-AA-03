6eeefc6e50b8c55bb23e210fa87f4603554f00ee
zach131 <zacharyarenson@gmail.com>
2019-11-20 01:23:49 -0800

    Update Shinypage.R

--- Shinypage.R
+++ Shinypage.R
@@ -1,5 +1,25 @@
-install.packages("shiny")
 library("shiny")
-ui <- fluidPage()
 
-server <-
+ui <- fluidPage(
+  titlePanel("Sea Star Population"),
+  
+  mainPanel( h3("Sea Star Ecosystem Analysis")
+  ),
+  
+  page_one <- tabPanel("Question 1: Does the rising water temperatures along the Western Coast of the United
+    States have any impact on the Sea Star population?"),
+  page_two <- tabPanel("Question 2: With recent numbers in Sea Star population dwindling, how will the shallow
+    ecosystems be affected and how much will that change the ocean?"),
+  page_three <- tabPanel("Question 3: How does the population of the other animals in the ecosystem appear to affect the sea star population?"),
+  page_four <- tabPanel("Question 4: Comparing the global warming temperatures to the general ecosystem where sea stars reside, is there a noticeable trend?"),
+  page_five <- tabPanel("Question 5: What correlation is found between the sea level and heat maps from the following datasets."),
+  page_six <- tabPanel("Question 6: When comparing sea level and the oceans ecosystem populations, is there a noticeable trend?")
+)
+ui <- navbarPage(
+  "My Application", # application title
+  page_one,         # include the first page content
+  page_two,         # include the second page content
+  page_three        # include the third page content
+)
+server <- function(input, output)
+  shinyApp(ui = my_ui, server = my_server)
