2d03ffb1b0d85c3c87c3f655d468151bd5272102
zach131 <zacharyarenson@gmail.com>
2019-11-20 06:50:18 -0800

    Update app.R

--- app.R
+++ app.R
@@ -4,23 +4,45 @@ library(shiny)
 ui <- fluidPage(
 =======
 library("shiny")
-  page_one <- tabPanel(
+  page_one <- tabPanel("First Page",
 >>>>>>> 371daaf305063397a65af35476c10aebe69faee9:Shinypage.R
   titlePanel("Sea Star Population"),
   "Question 1: Does the rising water temperatures along the Western Coast of the United
     States have any impact on the Sea Star population?"),
-  page_two <- tabPanel("Question 2: With recent numbers in Sea Star population dwindling, how will the shallow
-    ecosystems be affected and how much will that change the ocean?"),
-  page_three <- tabPanel("Question 3: How does the population of the other animals in the ecosystem appear to affect the sea star population?"),
-  page_four <- tabPanel("Question 4: Comparing the global warming temperatures to the general ecosystem where sea stars reside, is there a noticeable trend?"),
-  page_five <- tabPanel("Question 5: What correlation is found between the sea level and heat maps from the following datasets."),
-  page_six <- tabPanel("Question 6: When comparing sea level and the oceans ecosystem populations, is there a noticeable trend?")
+  sidebarLayout(
+    sidebarPanel(
+      
+    ),
+    mainPanel(
+      h3("Primary Content"),
+      p("Plots, data tables, etc. would go here")
+    )
+  )
 )
+page_two <- tabPanel(
+"Question 2: With recent numbers in Sea Star population dwindling, how will the shallow
+    ecosystems be affected and how much will that change the ocean?"
+)
+page_three <- tabPanel(
+  "Question 3: How does the population of the other animals in the ecosystem appear to affect the sea star population?"
+  )
+page_four <- tabPanel(
+  "Question 4: Comparing the global warming temperatures to the general ecosystem where sea stars reside, is there a noticeable trend?"
+  )
+page_five <- tabPanel(
+  "Question 5: What correlation is found between the sea level and heat maps from the following datasets."
+  )
+page_six <- tabPanel(
+  "Question 6: When comparing sea level and the oceans ecosystem populations, is there a noticeable trend?"
+  )
 ui <- navbarPage(
   "My Application", # application title
   page_one,         # include the first page content
   page_two,         # include the second page content
-  page_three        # include the third page content
+  page_three,
+  page_four,
+  page_five,
+  page_six
 )
 server <- function(input, output) {
   
