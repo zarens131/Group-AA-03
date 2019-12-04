1a7c9c039e361d98f16c83f584cf9c7193da158e
nrwick <nickrwicklund@gmail.com>
2019-12-03 18:54:17 -0800

    fixed plots, should be working

--- docs/ocean_heat_plot.R
+++ docs/ocean_heat_plot.R
@@ -2,19 +2,16 @@ heat_plot <- function(year1 = 1955, year2 = 2015) {
   file <- read.csv("docs/ocean-heat_fig-1.csv",
                    stringsAsFactors = FALSE)
   
+  
   ocean_heat <- ggplot(file, aes(x = Year, y = NOAA)) +
-    geom_point() +
-    geom_ribbon(aes(ymax = NOAA, ymin = NOAA),
-                fill = "yellow",
-                alpha = 0.5) +
-    geom_line(aes(x = Year, y = NOAA, color = "Ocean Heat")) +
-    scale_color_manual(values = c("darkred", "blue")) +
-    scale_y_continuous(breaks = seq(0, 12, 2)) +
-    ylab("NOAA")
-  
-  sea_level_plot <- sea_level_plot +
+    geom_bar(stat="identity", position=position_dodge(), fill = "darkred")+
+    theme_minimal()
+    
+  ocean_heat <- ocean_heat +
     xlim(year1, year2)
-  
+    
   return(ocean_heat)
+  
+  # geom_text(aes(label = NOAA), vjust = 1.6, size = 1.5) <--- NOT NEEDED 
 }
 
