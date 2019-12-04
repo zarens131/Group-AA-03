bb949b0d91b6e6eea7be1579a7ca3bc4cbed33c2
Arya Goel <aryagoel1@gmail.com>
2019-12-03 18:19:05 -0800

    ADDED

--- ocean_heat.r
+++ ocean_heat.r
@@ -4,8 +4,12 @@ library(ggplot2)
 file <- read.csv('docs/ocean-heat_fig-1.csv', stringsAsFactors = FALSE)
 
 bar_plot <- ggplot(file, aes(x = Year, y = NOAA)) +
-  geom_bar(stat="identity", position=position_dodge(), fill = "darkred")+
-  geom_text(aes(label = NOAA), vjust = 1.6, size = 1.5) +
-  theme_minimal()
-bar_plot
-
+            geom_line(aes(x = Year, y = NOAA, color = "Ocean Heat")) +
+            geom_point() +
+            geom_ribbon(aes(ymax = Year + interval, ymin = Year - interval),
+                        fill = "grey70",
+                        alpha = 0.5) +
+            geom_line(aes(x = Year, y = zero, color = "Normal Ocean Heat at 1955")) +
+            scale_color_manual(values = c("black", "darkred")) +
+            scale_x_continuous(breaks = seq(1955, 2015, 20))+ 
+            scale_y_continuous(breaks = seq(0, 12, 2))
