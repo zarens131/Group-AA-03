347c082b44d36302e950b4d85937b82e2ddf081f
zach131 <zacharyarenson@gmail.com>
2019-12-03 22:30:47 -0800

    Update app.R

--- app.R
+++ app.R
@@ -16,23 +16,37 @@ ui <- fluidPage(
     br(),
     sidebarLayout( 
       sidebarPanel(
-        h3("Technology:"),
-        p("Text here.")
+        h3("Technology"),
+        p("Each visualization is a unique interactable map that displays data in a creative way. 
+          Clicking on the dots will provide information that correlates to the image shown on the map. 
+          The drop-down list is a simple time frame that lapses over our data sets years. 
+          The sliding bars are used to show large amounts of data and to hone in on the requested 
+          time frames over the decades shown in the later questions.")
       ),
       mainPanel(leafletOutput("starfish_map")) #displays interactive map
     ),
       mainPanel(
       h3("Conclusion Statement"),
-      p("Conclusion Statement Paragraphs")
+      p("Our group has found a sudden and rapid change in the sea star population from 2010 to 2017, even possibly the present.
+        What we have found that could lead to such a drastic change are 
+        global ecosystem issues that definently have correlation.
+        We cannot conclude whether the ecosystems are negatively or 
+        positively affected by the rising of sea levels and ocean temperature.
+        However, we can conclude that this drastic affect from global warming 
+        has changed the ecosystem to negatively impact certain populations like the sea star.
+        Understanding correlations and recognizing a huge problem within our world is an important step towards our goals as researchers. 
+        Even though we have not reached a definite conclusion to the issue at hand. 
+        We have smaller conclusions such as recognizing the main problem, finding correlations between data on 
+        commonly known world issues, and finding a lack of data that can be expanded upon. Essentially, 
+        we can improve the researching field of sea star population data 
+        by being the first to organize such a nuanced and scattered issue.")
     )
   )
 )
 
 page_one <- tabPanel(
   "Question 1", titlePanel(
-    "Are the rising water temperatures along the Western Coast of the United
-  States having an impact on the Sea Star population?"),
-  h2("Research Questions and findings"),
+    "Are the sea star population of the west coast currently being affected by a change in their ecosystem?"),
   sidebarLayout(
     sidebarPanel(
       h3("Navigation Bar"),
@@ -42,6 +56,11 @@ page_one <- tabPanel(
                   selected = "2007")
     ),
     mainPanel(leafletOutput("map_interact"))
+  ),
+  h3("Research Questions and Findings"),
+  mainPanel(
+    p("Absolutely. We estimate that sometime during 2010 to 2011 that sea star populations rapidly began to spread and ultimately decline.
+      The sea star populations, based on our data from up to 2017, have scattered into four laid (points) on our project data page. ")
   )
 )
 page_two <- tabPanel(
@@ -49,7 +68,6 @@ page_two <- tabPanel(
     "Comparing the global warming temperatures to the general
   ecosystem where sea stars reside, is there a noticeable trend?"),
   titlePanel("Ocean Heat Graph"),
-  h2("Research Questions and Findings"),
   sidebarLayout(
     sidebarPanel(
       h3("Navigation Bar"),
@@ -57,13 +75,19 @@ page_two <- tabPanel(
                   max = 2015, value = c(1955, 2015))
     ),
     mainPanel(plotOutput("bar_plot"))
+  ),
+  h3("Research Questions and Findings"),
+  mainPanel(
+    p("Yes, there has been a sudden and very rapid decline in the sea star population 
+      that we can estimate happened around 2010 to 2011 based on our datasets. 
+      This leads our group findings to associate the change in the ecosystem to affect a 
+      certain predator thriving or a lack of food within the studied ecosystem.")
   )
 )
 page_three <- tabPanel(
   "Question 3", titlePanel(
     "What correlation is found between the sea level
   and heat maps from the following datasets?"),
-  h2("Research Questions and findings"),
   titlePanel("Sea Level Graph"), #displays banner title
   sidebarLayout(
     sidebarPanel(
@@ -72,6 +96,13 @@ page_three <- tabPanel(
                   max = 2015, value = c(1880, 2015))
     ),
     mainPanel(plotOutput("sealevels"))
+  ),
+  h3("Research Questions and Findings"),
+  mainPanel(
+    p("Our group findings concluded that there is a correlation between the sea level 
+    from question 3 and the surface heat map from question 2. 
+    From at least the mid 1950's, there has been a rising temperature spike 
+    and sea level spike that has affected the oceans ecosystems drastically over the decades. ")
   )
 )
 page_four <- tabPanel(
@@ -103,6 +134,10 @@ page_four <- tabPanel(
       p("Being interested in human influence towards the sea star population. 
         Arya took on the task of leading our research efforts, 
         challenging difficult scenarios in our project, and bringing new ideas to the table.")
+    ),
+    mainPanel(
+      h3("Link to Technical Document: "),
+      p("https://github.com/zarens131/Group-AA-03/wiki/Sea-Star-Technical-Document")
     )
   )
 )
