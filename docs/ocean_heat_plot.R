7a691d98d9dac2adcf8243ac1c0c9cb6e435501e
Arya Goel <aryagoel1@gmail.com>
2019-12-03 19:18:21 -0800

    ADDED

--- docs/ocean_heat_plot.R
+++ docs/ocean_heat_plot.R
@@ -1,20 +1,20 @@
-heat_plot <- function(year1 = 1955, year2 = 2015) {
+bar_plot <- function(year1 = 1955, year2 = 2015) {
   file <- read.csv("docs/ocean-heat_fig-1.csv",
                    stringsAsFactors = FALSE)
   
-  ocean_heat <- ggplot(file, aes(x = Year, y = NOAA)) +
+  bar_plot <- ggplot(file, aes(x = Year, y = NOAA)) +
     geom_point() +
     geom_ribbon(aes(ymax = NOAA, ymin = NOAA),
                 fill = "yellow",
                 alpha = 0.5) +
     geom_line(aes(x = Year, y = NOAA, color = "Ocean Heat")) +
     scale_color_manual(values = c("darkred", "blue")) +
-    scale_y_continuous(breaks = seq(0, 12, 2)) +
+    scale_y_continuous(breaks = seq(-7, 17, 2)) +
     ylab("NOAA")
   
-  sea_level_plot <- sea_level_plot +
+  bar_plot <- bar_plot +
     xlim(year1, year2)
   
-  return(ocean_heat)
+  return(bar_plot)
 }
 
--- ocean_heat.r
+++ ocean_heat.r
@@ -4,12 +4,13 @@ library(ggplot2)
 file <- read.csv('docs/ocean-heat_fig-1.csv', stringsAsFactors = FALSE)
 
 bar_plot <- ggplot(file, aes(x = Year, y = NOAA)) +
-            geom_line(aes(x = Year, y = NOAA, color = "Ocean Heat")) +
             geom_point() +
-            geom_ribbon(aes(ymax = Year + interval, ymin = Year - interval),
-                        fill = "grey70",
+            geom_ribbon(aes(ymax = NOAA, ymin = NOAA),
+                        fill = "yellow",
                         alpha = 0.5) +
-            geom_line(aes(x = Year, y = zero, color = "Normal Ocean Heat at 1955")) +
-            scale_color_manual(values = c("black", "darkred")) +
-            scale_x_continuous(breaks = seq(1955, 2015, 20))+ 
-            scale_y_continuous(breaks = seq(0, 12, 2))
+            geom_line(aes(x = Year, y = Year, color = "Ocean Heat")) +
+            scale_color_manual(values = c("darkred", "blue")) +
+            scale_y_continuous(breaks = seq(-7, 17, 2))
+            ylab("NOAA")
+            
+            
